d751b3ad328e13f48743dfa339b15ae79e296de5
GwenVS <gwendolene.vansteenkiste@student.kdg.be>
2017-03-17 10:51:52 +0100

    adding classes Seppe

---
+++ IedersStemTeltPOC/BL/IEigenschapManager.cs
@@ -0,0 +1,20 @@
+﻿using IST.BL.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IST.BL
+{
+    public interface IEigenschapManager
+    {
+        //Eigenschap GetEigenschap(int eigenschapId);
+        //ICollection<Eigenschap> GetEigenschappen();
+        Eigenschap AddEigenschap(int eigenschapId, string eigenschapNaam, ICollection<string> mogelijkheden, ICollection<Voorwaarde> voorwaarden);
+        Voorwaarde AddVraagVoorwaarde(int voorwardeId, Eigenschap eigenschap, string waarde, Vraag vraag);
+        Voorwaarde AddResultaatVoorwaarden(int voorwardeId, Eigenschap eigenschap, string waarde, Resultaat resultaat);
+        //void ChangeEigenschap(Eigenschap eigenschap);
+        //void RemoveEigenschap(int eigenschapId);
+    }
+}
