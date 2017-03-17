1ee486f0c18d2f69d6379c8cf7b2bd60bbceb8d5
GwenVS <gwendolene.vansteenkiste@student.kdg.be>
2017-03-17 10:53:34 +0100

    name change
    'POC' verwijderd

---
+++ IedersStemTelt/Domain/PartijStandpunt.cs
@@ -0,0 +1,10 @@
+﻿namespace IST.BL.Domain
+{
+    public class PartijStandpunt
+    {
+        public int PartijStandpuntId { get; set; }
+        public Thema Thema { get; set; }
+        public Partij Partij { get; set; }
+        public string StandpuntTekst { get; set; }
+    }
+}
