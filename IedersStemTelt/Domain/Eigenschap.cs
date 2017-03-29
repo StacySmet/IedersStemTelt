2b451ffe1c354c6a5fa5f09f9f9ea41483aa8887
GwenVS <gwendolene.vansteenkiste@student.kdg.be>
2017-03-29 13:55:24 +0200

    changing eigenschap-voorwaarde relatie
    ipv eigenschap Collection mee te geven, gewoon key-value

--- IedersStemTelt/Domain/Eigenschap.cs
+++ IedersStemTelt/Domain/Eigenschap.cs
@@ -8,10 +8,10 @@ namespace IST.BL.Domain
     {
         public int EigenschapId { get; set; }
         public string EigenschapNaam { get; set; }
-        public ICollection<string> Mogelijkheden { get; set; }
+        public string Value { get; set; }
 
 
-        //noodzakelijk?? zie Voorwaarde-klasse // correct?? Voorwaarde abstract?
+        //noodzakelijk?? zie Voorwaarde-klasse // correct?? Voorwaarde abstract?  // mss beter 3 lijsten met subklassen?
         public ICollection<Voorwaarde> Voorwaarden { get; set; }
     }
 }
